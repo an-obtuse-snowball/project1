4475c6e395bbc9a7c0a2651bac8250e9464b5569
an-obtuse-snowball <OriginalZenix@gmail.com>
2021-05-15 11:39:28 +0100

    Updated various parts.

--- libs/js/main.js
+++ libs/js/main.js
@@ -1,22 +1,67 @@
-function setCountryState() {
+/* Instantiate the object with raw data*/
+countryObject = {
+    countryName: "No Country known", //Replace with function to instantiate
+    capitalName: "Capital City is unknown", //Replace with function to instantiate
+    latitude: "Unknown latitude",
+    longitude: "Unknown Longitude",
+    currency: "Unknown currency",    
+    weather : {
+        Temperature: 0,
+        Humidity: 0,
+        Precipitation: 0
+    }
+};
 
-        //Use the API to initialise these to current location. 
-       var countryObject = {
-            countryName: "United Kingdom",
-            capitalName: "London",
-            latitude: -41.2,     //These are the 
-            longitude: 26.1,    //two main deliminators for determining the rest of the data.
-            currency: "GBP",
-            weather : {
-                Temperature: 14.2,
-                Humidity: 56,
-                Precipitation: 82
+/* Configuration Options for GeoLocation */
+var geoOptions = {
+    enableHighAccuracy: true,
+    timeout: 2000,
+    maximumAge: 0
+};
+
+function success(pos) {
+    countryObject.latitude = pos.coords.latitude;
+    countryObject.longitude = pos.coords.longitude;
+    console.log("Successfully acquired device location, updating current data to object.")
+    console.log("Current Longitude: "+countryObject.latitude);
+    console.log("Current Latitude: "+countryObject.latitude);
+}
+
+function error() {
+    console.log("Failed to get geolocation from device");
+}
+
+function handleCountryJSON() {
+    $.ajax({
+        url: "./libs/js/getCountryList.php",
+        type: 'POST',
+        dataType: "json",
+        data,
+
+        success: function(result) {
+            console.log("Ajax request success");
+            console.log(JSON.stringify(result));
+            if(result.status.name == "ok") {
+                console.log(result);
             }
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.log(jqXHR);
+            console.log(textStatus);
+            console.log (errorThrown);
         }
-return countryObject;
-    };
-console.log(setCountryState());
+    });
+
+    console.log(data);
 
 
+
+
+};
+//Requests location and updates the country Object
+navigator.geolocation.getCurrentPosition(success, error, geoOptions);
+
+//Updates the country list to choose from the countries.json
+handleCountryJSON();
 
 
